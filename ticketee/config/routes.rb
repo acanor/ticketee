2b798ebae33e73b9d4b0d9315295e6c5c74395dd
acanor <acanor@gmail.com>
2014-12-10 12:02:47 -0500

    'Creando un nuevo proyecto' aspecto completado.

--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,9 +1,10 @@
-Rails.application.routes.draw do
+Ticketee::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root "projects#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,7 +14,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :projects
   # Example resource route with options:
   #   resources :products do
   #     member do
