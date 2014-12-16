8b4a74f7aa0168cbedc0191f168e0d4ca6026b21
acanor <acanor@gmail.com>
2014-12-16 17:14:44 -0500

    Implementando pagines de perfil

--- ticketee/app/controllers/users_controller.rb
+++ ticketee/app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
-  	def new
+  	before_action :set_user, only: [:show, :edit, :update]
+
+	def new
   		@user = User.new
 	end
 
   	def create
 		@user = User.new(user_params)
-
+		
 		if @user.save
 			flash[:notice] = "You have signed up successfully."
 			redirect_to projects_path
@@ -17,8 +19,24 @@ class UsersController < ApplicationController
   	def show
   	end
 
+	def edit
+	end
+
+	def update
+		@user.update(user_params)
+
+		flash[:notice] = "Profile has been updated."
+		redirect_to @user
+	end
 	private
 		def user_params
 			params.require(:user).permit(:name, :password, :password_confirmation)
+		end
+
+		def set_user
+			@user = User.find(params[:id])
+			rescue ActiveRecord::RecordNotFound
+			flash[:alert] = "El usuario que está buscando no puede ser encontrado"
+			redirect_to users_path
 		end
 end
---
+++ ticketee/spec/factories/user_factory.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+	factory :user do
+		name "new_username"
+		email "user@example.com"
+		password "password"
+	end
+end
