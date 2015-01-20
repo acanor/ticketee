379e89c254136ed857fcedb46ad0abc3afaeab0f
acanor <acanor@gmail.com>
2015-01-20 15:52:23 -0500

    Agregando el atributo admin al usuario.

--- ticketee/spec/factories/user_factory.rb
+++ ticketee/spec/factories/user_factory.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name "new_username"
-		email "user@example.com"
-		password "password"
+		name "username"
+		email "sample@example.com"
+		password "hunter2"
+		password_confirmation = "hunter2"
+
+		factory :admin_user do
+			admin true
+		end
 	end
 end
