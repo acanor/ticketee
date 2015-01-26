34f52464410c4b8ea22d21f52bfb2daeddec1627
acanor <acanor@gmail.com>
2015-01-26 10:44:41 -0500

    Agregando la disponibilidad de crear usuarios a traves de la cuenta de administrador. Casi que no

--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,5 +1,10 @@
 Ticketee::Application.routes.draw do
   
+  namespace :admin do
+  	root :to => "base#index"
+	resources :users
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
