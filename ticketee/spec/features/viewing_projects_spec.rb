f5f469f18117085407ae10d34624f704d5070277
acanor <acanor@gmail.com>
2015-02-16 13:37:15 -0500

    No muestra proyectos que no tenga permisos de usar

--- ticketee/spec/features/viewing_projects_spec.rb
+++ ticketee/spec/features/viewing_projects_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 feature "Viewing projects" do
+
+	
 	let!(:user) { FactoryGirl.create(:user) }
 	let!(:project) { FactoryGirl.create(:project) }
 
@@ -10,6 +12,12 @@ feature "Viewing projects" do
 	end
 
 	scenario "Listing all projects" do
+		FactoryGirl.create(:project, name: "Hidden")
+	   visit '/'
+	   expect(page).to_not have_content("Hidden")
+	   click_link project.name
+
+
 		visit '/'
 		click_link project.name
 		expect(page.current_url).to eql(project_url(project))
